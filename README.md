ebd0c9d402660831967a6effb30e98ed2f45402d
Italoo220 <italosilava76@gmail.com>
2023-10-16 15:03:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# atividade04.py
+# atividade04
+# atividade04
+# atividade04.py
---
+++ atiidade04.py
@@ -0,0 +1,10 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+
+
+numero = float(input("digite  um numero: "))
+antercessor = numero -1
+print(antercessor)
+sucessor = numero + 1
+print(sucessor)
+
+
